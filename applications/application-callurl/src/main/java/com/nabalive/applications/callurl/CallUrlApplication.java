45b732be1bad7f9df377865cb28c44ac487ad00f
Julien Cheype <julien.cheype@mindscape.fr>
2011-12-19 15:22:50 +0100

    add call url

---
+++ applications/application-callurl/src/main/java/com/nabalive/applications/callurl/CallUrlApplication.java
@@ -0,0 +1,77 @@
+package com.nabalive.applications.callurl;
+
+import com.nabalive.application.core.ApplicationBase;
+import com.nabalive.common.server.MessageService;
+import com.nabalive.data.core.model.ApplicationConfig;
+import com.nabalive.data.core.model.Nabaztag;
+import com.ning.http.client.AsyncCompletionHandler;
+import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.AsyncHttpClientConfig;
+import com.ning.http.client.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.Executors;
+
+@Component("callurl")
+public class CallUrlApplication extends ApplicationBase {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private AsyncHttpClient asyncHttpClient = new AsyncHttpClient(
+            new AsyncHttpClientConfig.Builder()
+                    .setCompressionEnabled(true)
+                    .setAllowPoolingConnection(true)
+                    .setMaximumConnectionsTotal(1024)
+                    .setRequestTimeoutInMs(5000)
+                    .setConnectionTimeoutInMs(5000)
+                    .setIdleConnectionInPoolTimeoutInMs(30000)
+                    .setMaxRequestRetry(3)
+                    .setExecutorService(Executors.newFixedThreadPool(8))
+                    .build()
+    );
+    
+    @Override
+    public String getApikey() {
+        return "400A9E89-21F5-43F0-B8A6-24A64F8C8A0E";
+    }
+
+    @Autowired
+    private MessageService messageService;
+
+    private void doGet(String url) throws IOException {
+        asyncHttpClient.prepareGet(url).execute(new AsyncCompletionHandler() {
+            @Override
+            public Object onCompleted(Response response) throws Exception {
+                return response;
+            }
+        });
+
+    }
+    
+    @Override
+    public void onStartup(Nabaztag nabaztag, ApplicationConfig applicationConfig) throws Exception {
+
+        String urlToCall = applicationConfig.getParameters().get("callurl").get(0);
+        String method = applicationConfig.getParameters().get("method").get(0);
+        String parameters = applicationConfig.getParameters().get("parameters").get(0);
+
+        if("GET".equalsIgnoreCase(method)){
+            doGet(urlToCall+"?"+parameters);
+        }
+        else if( "POST".equalsIgnoreCase(method)){
+            doPost(urlToCall, parameters);
+        }
+    }
+
+    private void doPost(String urlToCall, String parameters) throws IOException {
+        asyncHttpClient.preparePost(urlToCall).setBody(parameters).execute(new AsyncCompletionHandler() {
+            @Override
+            public Object onCompleted(Response response) throws Exception {
+                return response;
+            }
+        });
+    }
+}
